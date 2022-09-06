d21aa56b00bea018bad32416829cf12d3c82aae3
Newtonkamau14 <newtonkamau14@gmail.com>
2022-09-06 23:04:40 +0300

    adding address page

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const cors = require('cors')
-const bookController = require('./routes/bookController');
-const animeController = require('./routes/animeController');
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 3000;
 const app = express()
 
 
 //Middleware
-app.use(expressLayouts)
-app.set('view engine','ejs')
-app.set('layout','layouts/layout')
-app.use(express.static('public'))
-app.use(cors())
+app.use(expressLayouts);
+app.set('view engine','ejs');
+app.set('layout','layouts/layout');
+app.use(express.static('public'));
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-//Routes
-app.use('/books',bookController);
-app.use('/anime',animeController);
+//Route prefix
+app.use('',require('./routes/route'));
 
-//Homepage
-app.get('/',(req,res) => {
-    res.render('pages/home',{
-        title: 'Home'
-    })
-});
 
 //Listening on port 3000
 app.listen(PORT,() => {
-    console.log('Listening on port 3000')
+    console.log(`Listening on port at http://localhost:${PORT}`)
 })
